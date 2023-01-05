5a3fbd59e3e8f2eef0a2a5d1fca2e13758f56c27
BeLiHSBO <73900312+BLiHSBO@users.noreply.github.com>
2023-01-05 16:26:31 +0100

    feat: header-clock working

--- main.py
+++ main.py
@@ -14,24 +14,35 @@ class Main:
     def setup(self):
         self._datacollector_controller.setup()
         self.display = Display(21, 22)
+
+        # sync system-time via ntp
+        try:
+            # https://forum.micropython.org/posting.php?mode=quote&f=15&p=29387
+            # micropython offers no timezone support - only GMT
+            import ntptime
+            import time
+
+            ntptime.host = "pool.ntp.org"
+            ntptime.settime()
+        except:
+            print("Error syncing time")
         
         self._async_loop.run_until_complete(self.main())
 
     async def main(self):
         while True:
-            self.display.header("00:00", None)
+            self.display.header()
             self.display.drawMeasurement("Temperatur", 23.22)
             await asyncio.sleep(1)
-            self.display.header("23:59", -100)
+            self.display.header(wifidBm = -100)
             self.display.drawMeasurement("Swag", "100 %")
             await asyncio.sleep(1)
-            self.display.header("01:23", -50)
+            self.display.header(wifidBm = -50)
             self.display.drawMeasurement("Feuchte", 0.5124)
             await asyncio.sleep(1)
-            self.display.header("13:37", -5)
+            self.display.header(wifidBm = -5)
             self.display.drawMeasurement("daswirdzulangseinseinsein", "20 cm")
             await asyncio.sleep(1)
-            
 
     def get_async_loop(self):
         return self._async_loop
